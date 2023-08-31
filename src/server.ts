f8c47de692cdadca8b6e3c3ec96fbd1ce08d66de
mgckaled <marcelgckaled@gmail.com>
2023-08-31 07:08:18 -0300

    chore: configure eslint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 // instanciar a base da aplicação através da principal função
 const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello World"
+app.get('/hello', () => {
+  return 'Hello World'
 })
 
-app
+void app
   .listen({
-    host: "0.0.0.0",
-    port: 3333,
+    host: '0.0.0.0',
+    port: 3333
   })
   .then(() => {
-    console.log("🚧 HTTP Server Running! 🚀")
+    console.log('🚧 HTTP Server Running! 🚀')
   })
